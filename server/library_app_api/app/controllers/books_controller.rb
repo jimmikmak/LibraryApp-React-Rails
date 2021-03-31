fc3d7e047c91bc9291dc95e4cdbb8ba4749e15aa
jimmikmak <73326512+jimmikmak@users.noreply.github.com>
2021-03-31 17:36:26 +1100

    CRUD controllers working

--- server/library_app_api/app/controllers/books_controller.rb
+++ server/library_app_api/app/controllers/books_controller.rb
@@ -1,21 +1,31 @@
 class BooksController < ApplicationController
   def index
-    render json: { type: 'index' }
+    render json: Books.all
   end
 
   def create
-    render json: { type: 'create' }
+    book = Books.create(title: params[:title], author: params[:author])
+    book_valid = book.valid?
+    if book_valid
+      render json: { type: 'Successfully created book list!' }, status: 200
+    else
+      render json: { message: 'Unable to create book list' }, status: 400
+    end
   end
 
   def show
-    render json: { type: 'show' }
+    puts params[:id]
+    render json: Books.find(params[:id])
   end
 
   def update
-    render json: { type: 'update' }
+    book = Books.find(params[:id])
+    book.update(title: params[:title], author: params[:author])
+    render json: { type: 'Successfully updated book!' }
   end
 
   def destroy
-    render json: { type: 'destroy' }
+    Books.destroy(params[:id])
+    render json: { type: 'Book has been deleted!!' }
   end
 end
